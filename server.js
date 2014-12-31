dfa6b19de7c990291f26f83370f2d3ce60094841
ivorysmoker <deriaz.yannick@bluewin.ch>
2014-12-31 16:22:09 +0100

    Revert "Update server.js"
    This reverts commit eb133edf08d06c75e51dc556b6f4eb238a7b8ad6.

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ console.log("Ein Benutzer ist wiedergekehrt und hat die POS"+ pos);
 socket.nickname = BenutzerIpName[pos];
 socket.PlayerActive = 0;
 socket.PlayerPosition = 0;
-socket.PlayerCash = 1500;
+socket.PlayerCash = 1000;
 OnlineUsers[socket.nickname] = socket;
 updateNicknamesOnline();
 }
@@ -132,8 +132,7 @@ updateNicknamesOnline();
 		}
 
 		if(num1 === num2) {
-			socket.paschcounter = socket.paschcounter + 1;
-			console.log("Pasch! Next");
+			socket.paschcounter = socket.paschcounter + 1;			console.log("Pasch! Next");
 			//console.log("pasch juhu darf nochma!"+socket.paschcounter);
 			if (socket.paschcounter === 3) {
 			    console.log("ab ins jail mit dir!");	
